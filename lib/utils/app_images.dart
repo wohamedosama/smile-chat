70ebf0233a434b613158d5cc06211f377bff5d2d
mohamed osama <mohamedmedo9932@gmail.com>
2025-04-04 12:09:19 +0200

    add the assets in the application and generate it

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,117 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAccountIcons
+  /// assets/images/account_icons.svg
+  static const String assetsImagesAccountIcons = "assets/images/account_icons.svg";
+
+  /// Assets for assetsImagesBlock
+  /// assets/images/block.svg
+  static const String assetsImagesBlock = "assets/images/block.svg";
+
+  /// Assets for assetsImagesCall
+  /// assets/images/Call.svg
+  static const String assetsImagesCall = "assets/images/Call.svg";
+
+  /// Assets for assetsImagesCallIconIntheNavBar
+  /// assets/images/call_icon_inthe_nav_bar.svg
+  static const String assetsImagesCallIconIntheNavBar = "assets/images/call_icon_inthe_nav_bar.svg";
+
+  /// Assets for assetsImagesCamera
+  /// assets/images/Camera.svg
+  static const String assetsImagesCamera = "assets/images/Camera.svg";
+
+  /// Assets for assetsImagesContactIconIntheNavBar
+  /// assets/images/contact_icon_inthe_nav_bar.svg
+  static const String assetsImagesContactIconIntheNavBar = "assets/images/contact_icon_inthe_nav_bar.svg";
+
+  /// Assets for assetsImagesData
+  /// assets/images/Data.svg
+  static const String assetsImagesData = "assets/images/Data.svg";
+
+  /// Assets for assetsImagesDoc
+  /// assets/images/doc.svg
+  static const String assetsImagesDoc = "assets/images/doc.svg";
+
+  /// Assets for assetsImagesEnterOtpCodeImage
+  /// assets/images/enter_otp_code_image.svg
+  static const String assetsImagesEnterOtpCodeImage = "assets/images/enter_otp_code_image.svg";
+
+  /// Assets for assetsImagesHelp
+  /// assets/images/Help.svg
+  static const String assetsImagesHelp = "assets/images/Help.svg";
+
+  /// Assets for assetsImagesHomeIconIntheNavBar
+  /// assets/images/home_icon_inthe_nav_bar.svg
+  static const String assetsImagesHomeIconIntheNavBar = "assets/images/home_icon_inthe_nav_bar.svg";
+
+  /// Assets for assetsImagesLocation
+  /// assets/images/location.svg
+  static const String assetsImagesLocation = "assets/images/location.svg";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.svg
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+
+  /// Assets for assetsImagesLogout
+  /// assets/images/logout.svg
+  static const String assetsImagesLogout = "assets/images/logout.svg";
+
+  /// Assets for assetsImagesMedia
+  /// assets/images/media.svg
+  static const String assetsImagesMedia = "assets/images/media.svg";
+
+  /// Assets for assetsImagesMessage
+  /// assets/images/Message.svg
+  static const String assetsImagesMessage = "assets/images/Message.svg";
+
+  /// Assets for assetsImagesNewContact
+  /// assets/images/new_contact.svg
+  static const String assetsImagesNewContact = "assets/images/new_contact.svg";
+
+  /// Assets for assetsImagesNewGroup
+  /// assets/images/new_group.svg
+  static const String assetsImagesNewGroup = "assets/images/new_group.svg";
+
+  /// Assets for assetsImagesNotification
+  /// assets/images/notification.svg
+  static const String assetsImagesNotification = "assets/images/notification.svg";
+
+  /// Assets for assetsImagesPhoneAuthImage
+  /// assets/images/phone_auth_image.svg
+  static const String assetsImagesPhoneAuthImage = "assets/images/phone_auth_image.svg";
+
+  /// Assets for assetsImagesPoll
+  /// assets/images/poll.svg
+  static const String assetsImagesPoll = "assets/images/poll.svg";
+
+  /// Assets for assetsImagesQrCode
+  /// assets/images/qr_code.svg
+  static const String assetsImagesQrCode = "assets/images/qr_code.svg";
+
+  /// Assets for assetsImagesSearch
+  /// assets/images/Search.svg
+  static const String assetsImagesSearch = "assets/images/Search.svg";
+
+  /// Assets for assetsImagesSplashScreen
+  /// assets/images/splash_screen.svg
+  static const String assetsImagesSplashScreen = "assets/images/splash_screen.svg";
+
+  /// Assets for assetsImagesTrash
+  /// assets/images/Trash.svg
+  static const String assetsImagesTrash = "assets/images/Trash.svg";
+
+  /// Assets for assetsImagesUser
+  /// assets/images/user.svg
+  static const String assetsImagesUser = "assets/images/user.svg";
+
+  /// Assets for assetsImagesUsers
+  /// assets/images/Users.svg
+  static const String assetsImagesUsers = "assets/images/Users.svg";
+
+  /// Assets for assetsImagesVideo
+  /// assets/images/Video.svg
+  static const String assetsImagesVideo = "assets/images/Video.svg";
+}
+
