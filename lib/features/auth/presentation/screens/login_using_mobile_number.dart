4d16aa3cd818ab65a93c1ac221079d179bdb1fd6
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-04-25 03:46:50 +0300

    end from login using mobile number screen

--- lib/features/auth/presentation/screens/login_using_mobile_number.dart
+++ lib/features/auth/presentation/screens/login_using_mobile_number.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smile_chat/features/landing/presentation/widgets/custom_elevated_button.dart';
 import 'package:smile_chat/utils/app_color.dart';
+import 'package:smile_chat/utils/app_font_size.dart';
+import 'package:smile_chat/utils/app_images.dart';
 
 class LoginUsingMobileNumber extends StatelessWidget {
   const LoginUsingMobileNumber({super.key});
@@ -7,10 +11,122 @@ class LoginUsingMobileNumber extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColor.lightBlueColor,
-        title: const Text('Login Using Mobile Number'),
+      backgroundColor: AppColor.primayColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(22),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 15),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: SvgPicture.asset(
+                        height: 50,
+                        Assets.assetsImagesArrowBackIcon,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Spacer(),
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: Image.asset(
+                        fit: BoxFit.cover,
+                        Assets.assetsImagesLogo,
+                      ),
+                    )
+                  ],
+                ),
+                // SizedBox(height: 50),
+                Text(
+                  'Hello!',
+                  style: AppStyles.styleSemiBoldwhiteColor45,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'we are requesting your mobile number just to enter our platform for login purpose.',
+                  style: AppStyles.styleRegular16
+                      .copyWith(color: AppColor.whiteColor),
+                ),
+                SizedBox(height: 25),
+                Image.asset(
+                  Assets.assetsImagesPhoneAuthImage,
+                  fit: BoxFit.contain,
+                ),
+
+                TextFormField(
+                  style: AppStyles.styleMediumWithWhiteColor16,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(width: 12),
+                        Text(
+                          '+91',
+                          style: AppStyles.styleRegular16
+                              .copyWith(color: Color(0xffcccccc)),
+                        ),
+                        const Icon(
+                          Icons.arrow_drop_down,
+                          color: Color(0xffcccccc),
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                            width: 1, height: 24, color: Color(0xffcccccc)),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                    hintText: "Enter Mobile Number",
+                    hintStyle: AppStyles.styleRegular16
+                        .copyWith(color: Color(0xffcccccc)),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffcccccc)),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffcccccc)),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                CustomElevatedButton(
+                  onPressed: () {},
+                  text: 'Get OTP',
+                ),
+                SizedBox(height: 30),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: 'By signing ,you agree to our\n',
+                          style: AppStyles.styleMediumWithWhiteColor16),
+                      TextSpan(
+                          text: 'Terms of Service',
+                          style: AppStyles.styleMediumWithLightBlueColor16),
+                      TextSpan(
+                          text: ' &',
+                          style: AppStyles.styleMediumWithWhiteColor16),
+                      TextSpan(
+                          text: ' Privacy',
+                          style: AppStyles.styleMediumWithLightBlueColor16),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 80),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import firebase_storage
-import flutter_local_notifications
 import google_sign_in_ios
 import shared_preferences_foundation
 
@@ -24,7 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
