9581a12b6d71a9b8f4497e011bf11027540638dd
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-05-09 03:32:09 +0300

    refactor search item and create sample model to manage the search item

---
+++ lib/features/home/model/search_model.dart
@@ -0,0 +1,8 @@
+class SearchModel {
+  final String image, text, subtitle;
+  const SearchModel({
+    required this.image,
+    required this.text,
+    required this.subtitle,
+  });
+}
--- lib/features/home/presentation/screens/search_screen.dart
+++ lib/features/home/presentation/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smile_chat/features/home/presentation/widgets/search_list_view.dart';
 import 'package:smile_chat/utils/app_color.dart';
 import 'package:smile_chat/utils/app_font_size.dart';
 import 'package:smile_chat/utils/app_images.dart';
@@ -10,64 +11,50 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: AppColor.whiteColor,
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: SafeArea(
           child: Column(
             children: [
-              const SizedBox(height: 20),
-              TextFormField(
-                cursorColor: const Color(0xff24786D),
-                decoration: InputDecoration(
-                  suffixIcon: GestureDetector(
-                      onTap: () {},
-                      child: Image.asset(Assets.assetsImagesRemoveIcon)),
-                  labelText: 'People',
-                  labelStyle: AppStyles.styleMediam14
-                      .copyWith(color: AppColor.blackColor),
-                  prefixIcon: GestureDetector(
-                      onTap: () {},
-                      child: Image.asset(Assets.assetsImagesSearchIcon)),
-                  filled: true,
-                  fillColor: const Color(0xffF3F6F6),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: Color(0xffF3F6F6)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: Color(0xffF3F6F6)),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 25),
-              ListTile(
-                leading: CircleAvatar(
-                  radius: 22,
-                  child: Image.asset(
-                    Assets.assetsImagesPerson2,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                title: Text(
-                  'Adil Adnan',
-                  style: AppStyles.styleBold16
-                      .copyWith(color: AppColor.blackColor),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                subtitle: Text(
-                  'Be your own hero 💪',
-                  style: AppStyles.styleExtraBold16
-                      .copyWith(color: AppColor.midGrayColor),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
+              SearchTextFromField(),
+              SizedBox(height: 25),
+              SearchListView(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchTextFromField extends StatelessWidget {
+  const SearchTextFromField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: const Color(0xff24786D),
+      decoration: InputDecoration(
+        suffixIcon: GestureDetector(
+            onTap: () {}, child: Image.asset(Assets.assetsImagesRemoveIcon)),
+        labelText: 'People',
+        labelStyle:
+            AppStyles.styleMediam14.copyWith(color: AppColor.blackColor),
+        prefixIcon: GestureDetector(
+            onTap: () {}, child: Image.asset(Assets.assetsImagesSearchIcon)),
+        filled: true,
+        fillColor: const Color(0xffF3F6F6),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xffF3F6F6)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xffF3F6F6)),
         ),
       ),
     );
